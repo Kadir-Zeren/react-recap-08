f38120f6b7c3dd6dfe09d06a00c744d26fd3ff95
kadir-zeren <k4dzeren@gmail.com>
2024-03-05 18:36:51 +0300

    formObject

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Form from "./components/1-forms/Form";
+import FormObject from "./components/1-forms/FormObject";
 
 function App() {
   return (
     <div>
-      <Form />
+      {/* <Form /> */}
+      <FormObject />
     </div>
   );
 }
