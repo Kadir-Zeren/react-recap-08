462114c4afba110330ece5b3ab55dc2b9d3dfcbd
kadir-zeren <k4dzeren@gmail.com>
2024-03-05 19:04:03 +0300

    onCut

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Form from "./components/1-forms/Form";
 import FormObject from "./components/1-forms/FormObject";
+import KeyboardClipboard from "./components/keyboardClipboard/KeyboardClipboard";
 
 function App() {
   return (
     <div>
       {/* <Form /> */}
-      <FormObject />
+      {/* <FormObject /> */}
+      <KeyboardClipboard />
     </div>
   );
 }
